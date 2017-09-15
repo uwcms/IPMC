412cd80ac2c261381cd56c89ab483300aaea6334
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2017-09-15 10:48:45 -0500

    UWIPMC_INITIALIZE_STATIC_SEMAPHORE()
    Create a common macro in IPMC.h to handle boot-time initialization of
    FreeRTOS semaphores that are statically declared.  FreeRTOS seems to
    lack a static initializer macro that can simply be assigned to these
    types, requiring intialization with runtime code instead.
    
    This macro will create a function, automatically run before main(), that
    calls the appropriate FreeRTOS initializer.
    
    The priority of this initializer relative to other similar functions may
    change in the future, as other needs appear.

--- uw-ipmc/IPMC.h
+++ uw-ipmc/IPMC.h
@@ -10,4 +10,23 @@
 
 #include "libwrap.h" // Mutex wrappers for various non-reentrant stdlib functions.
 
+/*
+ * This macro will handle the automatic (at startup, before main) initialization of static semaphore objects.
+ *
+ * Parameters:
+ *   mutex:  The name of the previously defined SemaphoreHandle_t object.
+ *   type:   The semaphore type to create.  A xSemaphoreCreate{TYPE}Static FreeRTOS function must exist.
+ *
+ * Example:
+ *   static SemaphoreHandle_t libwrap_mutex = NULL;
+ *   UWIPMC_INITIALIZE_STATIC_SEMAPHORE(libwrap_mutex, Mutex);
+ */
+#define UWIPMC_INITIALIZE_STATIC_SEMAPHORE(sem, type) \
+	static void __uwipmc_initialize_semaphore_ ## sem(void) __attribute__((constructor(0))); \
+	static void __uwipmc_initialize_semaphore_ ## sem(void) { \
+		static StaticSemaphore_t buf; \
+		sem = xSemaphoreCreate ## type ## Static(&buf); \
+		configASSERT(sem); \
+	}
+
 #endif /* SRC_IPMC_H_ */
