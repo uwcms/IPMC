aca306eb8fbc4e6cda4cb5d04b294cf462c61646
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-01-04 13:51:31 -0600

    An EEPROM backed persistent storage service.

---
+++ uw-ipmc/services/persistentstorage/PersistentStorage.cpp
@@ -0,0 +1,302 @@
+/*
+ * PersistentStorage.cpp
+ *
+ *  Created on: Jan 2, 2018
+ *      Author: jtikalsky
+ */
+
+#include <services/persistentstorage/PersistentStorage.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
+#include <IPMC.h>
+#include <drivers/spi_eeprom/SPIEEPROM.h>
+#include <libs/SkyRoad.h>
+#include <string.h>
+
+#define NVREG32(baseptr, offset) (*((uint32_t*)((baseptr)+(offset))))
+
+static void run_persistentstorage_thread(void *cb_ps) {
+	reinterpret_cast<PersistentStorage*>(cb_ps)->run_flush_thread();
+}
+
+/**
+ * Instantiate a Persistent Storage module backed by the supplied EEPROM.
+ *
+ * @param eeprom  The EEPROM providing the backing for this module.
+ * @param logtree Where to send log messages from this module
+ */
+PersistentStorage::PersistentStorage(SPI_EEPROM &eeprom, LogTree &logtree)
+	: eeprom(eeprom), logtree(logtree) {
+	this->cache = (u8*)pvPortMalloc(this->eeprom.size*2 + 4);
+	NVREG32(this->cache, this->eeprom.size) = 0x1234dead; // Set Canary
+	this->data = this->cache + this->eeprom.size + 4;
+	this->logtree.log("Persistent storage task starting.", LogTree::LOG_INFO);
+	this->storage_loaded.clear();
+	this->flushwait[0] = new WaitList();
+	this->flushwait[1] = new WaitList();
+	this->index_mutex = xSemaphoreCreateMutex();
+	// We are a driver task until the initial load is complete, then will change to a background task.
+	configASSERT(xTaskCreate(run_persistentstorage_thread, "PersistentFlush", configMINIMAL_STACK_SIZE+256, this, TASK_PRIORITY_DRIVER, &this->flushtask));
+}
+
+PersistentStorage::~PersistentStorage() {
+	NVREG32(this->cache, this->eeprom.size) = 0; // Clear the canary for good measure.
+	vSemaphoreDelete(this->index_mutex);
+	delete this->flushwait[1];
+	delete this->flushwait[0];
+	configASSERT(0); // Unsupported, no way to safely shutdown run_flush_thread at the moment.
+}
+
+/**
+ * The global header for the persistent storage space.
+ */
+struct PersistentStorageHeader {
+	u16 version; ///< The version of this persistent storage format.
+};
+
+/**
+ * An entry in the persistent storage section index.
+ */
+struct PersistentStorageIndexRecord {
+	u16 id;      ///< The ID of the section.
+	u16 pgoff;   ///< The page number of the section start.
+	u16 pgcount; ///< The length in pages of the section.
+	u16 version; ///< The version of the section.
+};
+
+/**
+ * Return the current version of the specified section, or 0 if it does not exist.
+ *
+ * @param section_id The section to check the version of
+ * @return The section version, or zero if absent.
+ */
+u16 PersistentStorage::get_section_version(u16 section_id) {
+	this->storage_loaded.wait(portMAX_DELAY);
+	xSemaphoreTake(this->index_mutex, portMAX_DELAY);
+	struct PersistentStorageIndexRecord *index = reinterpret_cast<struct PersistentStorageIndexRecord *>(this->data + sizeof(struct PersistentStorageHeader));
+	for (int i = 0; index[i].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++i) {
+		if (index[i].id == section_id) {
+			xSemaphoreGive(this->index_mutex);
+			return index[i].version;
+		}
+	}
+	xSemaphoreGive(this->index_mutex);
+	return 0;
+}
+
+/**
+ * Set the current version of the specified section, if it exists.
+ *
+ * @param section_id The section that should be updated
+ * @param section_version The new version number of that section.
+ */
+void PersistentStorage::set_section_version(u16 section_id, u16 section_version) {
+	this->storage_loaded.wait(portMAX_DELAY);
+	xSemaphoreTake(this->index_mutex, portMAX_DELAY);
+	struct PersistentStorageIndexRecord *index = reinterpret_cast<struct PersistentStorageIndexRecord *>(this->data + sizeof(struct PersistentStorageHeader));
+	for (int i = 0; index[i].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++i)
+		if (index[i].id == section_id)
+			index[i].version = section_version;
+	xSemaphoreGive(this->index_mutex);
+}
+
+/**
+ * Retrieve the specified persistent storage section, allocating it if necessary.
+ *
+ * When retrieving a persistent storage, the supplied version and size must
+ * match the existing record or an error will occur.
+ *
+ * @param section_id  The ID of the section to allocate or retrieve.
+ * @param section_version  The version of the section to allocate or retrieve.
+ * @param section_size  The size of the section to allocate or retrieve.
+ * @return A pointer to a memory of size section_size, backed by persistent storage, or NULL on error.
+ */
+void *PersistentStorage::get_section(u16 section_id, u16 section_version, u16 section_size) {
+	configASSERT(section_id != PersistentStorageAllocations::RESERVED_END_OF_INDEX);
+	this->storage_loaded.wait(portMAX_DELAY);
+	xSemaphoreTake(this->index_mutex, portMAX_DELAY);
+	struct PersistentStorageIndexRecord *index = reinterpret_cast<struct PersistentStorageIndexRecord *>(this->data + sizeof(struct PersistentStorageHeader));
+
+	u16 section_pgcount = (section_size / this->eeprom.page_size) + (section_size % this->eeprom.page_size ? 1 : 0);
+	int i;
+	for (i = 0; index[i].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++i) {
+		if (index[i].id == section_id) {
+			if (index[i].version != section_version) {
+				this->logtree.log(stdsprintf("Version mismatch retrieving persistent storage section %hu: %hu requested, %hu present.", section_id, section_version, index[i].version), LogTree::LOG_ERROR);
+				xSemaphoreGive(this->index_mutex);
+				return NULL;
+			}
+			if (index[i].pgcount != section_pgcount) {
+				this->logtree.log(stdsprintf("Size mismatch retrieving persistent storage section %hu: %hu pages requested, %hu pages present.", section_id, section_pgcount, index[i].pgcount), LogTree::LOG_ERROR);
+				xSemaphoreGive(this->index_mutex);
+				return NULL;
+			}
+			xSemaphoreGive(this->index_mutex);
+			return this->data + (index[i].pgoff * this->eeprom.page_size);
+		}
+	}
+
+	/* If we got this far, the section doesn't exist yet.  We'll need to allocate it.
+	 *
+	 * index[i] is now the EOL marker.  We'll need to ensure we have space for
+	 * one additional record (the new EOL), and find a space to allocate the
+	 * storage section itself.
+	 *
+	 * One thing at a time.
+	 */
+	u16 minimum_address = sizeof(struct PersistentStorageHeader) + (i+2)*sizeof(struct PersistentStorageIndexRecord);
+	u16 minimum_page = (minimum_address / this->eeprom.page_size) + (minimum_address % this->eeprom.page_size ? 1 : 0);
+
+	// We start allocations from the end of EEPROM.
+	u16 allocpg = (this->eeprom.size/this->eeprom.page_size)-section_pgcount;
+	bool potential_overlap = true;
+	while (allocpg < minimum_page && potential_overlap) {
+		// Ok, find an overlap with this allocation.
+		potential_overlap = false; // Unless we find something below, we know there is no overlap.
+		for (int x = 0; index[x].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++x) {
+			if (( allocpg >= index[x].pgoff && !(allocpg >= index[x].pgoff + index[x].pgcount) ) /* our start is in this range */ ||
+				( allocpg+section_pgcount >= index[x].pgoff && !(allocpg+section_pgcount >= index[x].pgoff + index[x].pgcount) ) /* our end is in this range */ ) {
+				// We overlap.  Move us to before the start of this section.
+				potential_overlap = true;
+				if (index[x].pgoff < section_pgcount)
+					allocpg = 0; // No further space, we'll find our way out through the failure path.
+				else
+					allocpg = index[x].pgoff - section_pgcount;
+			}
+		}
+	}
+	if (allocpg < minimum_page) {
+		// We failed to find a valid allocation.
+		this->logtree.log(stdsprintf("Unable to allocate %hu contiguous pages for persistent storage section %hu.", section_pgcount, section_id), LogTree::LOG_ERROR);
+		xSemaphoreGive(this->index_mutex);
+		return NULL;
+	}
+	else {
+		// Record the allocation.
+		index[i].id = section_id;
+		index[i].pgoff = allocpg;;
+		index[i].pgcount = section_pgcount;
+		index[i].version = section_version;
+
+		// Mark the new terminator.
+		index[i+1].id = PersistentStorageAllocations::RESERVED_END_OF_INDEX;
+
+		xSemaphoreGive(this->index_mutex);
+		return this->data + (index[i].pgoff * this->eeprom.page_size); // The flush order will ensure we are flushed before any data in the new section.
+	}
+}
+
+/**
+ * Delete all instances of the specified persistent storage section.
+ *
+ * \note This will perform a blocking flush.
+ *
+ * @param section_id The ID of the section to delete.
+ */
+void PersistentStorage::delete_section(u16 section_id) {
+	this->storage_loaded.wait(portMAX_DELAY);
+	struct PersistentStorageIndexRecord *index = reinterpret_cast<struct PersistentStorageIndexRecord *>(this->data + sizeof(struct PersistentStorageHeader));
+	xSemaphoreTake(this->index_mutex, portMAX_DELAY);
+	for (int i = 0; index[i].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++i) {
+		if (index[i].id == section_id) {
+			this->logtree.log(stdsprintf("Deleting persistent storage allocation for section %hu (version %hu) at %hu, freeing %hu pages.", index[i].id, index[i].version, index[i].pgoff, index[i].pgcount), LogTree::LOG_NOTICE);
+			for (int x = i; index[x].id != PersistentStorageAllocations::RESERVED_END_OF_INDEX; ++x)
+				index[x] = index[x+1];
+			i--; // Recheck this index, it's not the same record anymore.
+		}
+	}
+	xSemaphoreGive(this->index_mutex);
+	this->flush(portMAX_DELAY); // We have to flush, to ensure that the index is consistent.
+}
+
+/**
+ * Flush cached EEPROM writes immediately.  If an optional timeout is specified,
+ * this call will wait until the flush has been completed, and the flush thread
+ * will inherit the priority of the calling task.
+ *
+ * @param timeout How long to wait for a synchronous flush.
+ * @return false if wait timed out, else true
+ */
+bool PersistentStorage::flush(TickType_t timeout) {
+	WaitList *wl = this->flushwait[0];
+	WaitList::Subscription_t sub = NULL;
+	if (timeout)
+		sub = wl->join();
+	this->logtree.log("Requesting explicit flush of persistent storage.", LogTree::LOG_DIAGNOSTIC);
+	xTaskNotifyGive(this->flushtask);
+	if (timeout) {
+		/* Inherit priority.  If the existing priority were already higher than
+		 * us, we wouldn't be running to decrease it.
+		 *
+		 * There is a race condition here, where we might have swapped out
+		 * between notifying the thread and increasing its priority, but the
+		 * cost of raising its priority after it has already done our flush pass
+		 * is not high, as it will auto-disinherit after the next background
+		 * pass, and there's no better way without a vTaskPrioritySetFromISR()
+		 * function that can be used in a critical section.
+		 */
+		unsigned long my_priority = uxTaskPriorityGet(NULL);
+		if (my_priority > TASK_PRIORITY_BACKGROUND)
+			vTaskPrioritySet(this->flushtask, my_priority);
+		return wl->wait(sub, timeout);
+	}
+	return true;
+}
+
+void PersistentStorage::run_flush_thread() {
+	this->logtree.log("Loading persistent storage.", LogTree::LOG_INFO);
+	this->eeprom.read(0, this->cache, this->eeprom.size);
+	memcpy(this->data, this->cache, this->eeprom.size);
+
+	struct PersistentStorageHeader *hdr = reinterpret_cast<struct PersistentStorageHeader*>(this->data);
+	if (hdr->version == 0 || hdr->version == 0xffff) {
+		// Uninitialized
+		hdr->version = 1;
+		struct PersistentStorageIndexRecord *secrec1 = reinterpret_cast<struct PersistentStorageIndexRecord*>(this->data + sizeof(struct PersistentStorageHeader));
+		secrec1->id = PersistentStorageAllocations::RESERVED_END_OF_INDEX; // Initialize the EOL marker.
+		this->logtree.log("Persistent storage first use initialization complete.", LogTree::LOG_NOTICE);
+	}
+	else if (hdr->version != 1) {
+		this->logtree.log(stdsprintf("Persistent storage version %hhu not recognized, persistent storage REFORMATTED.", hdr->version), LogTree::LOG_CRITICAL);
+		hdr->version = 1;
+		struct PersistentStorageIndexRecord *secrec1 = reinterpret_cast<struct PersistentStorageIndexRecord*>(this->data + sizeof(struct PersistentStorageHeader));
+		secrec1->id = PersistentStorageAllocations::RESERVED_END_OF_INDEX; // Initialize the EOL marker.
+	}
+
+	this->storage_loaded.set();
+	while (true) {
+		vTaskPrioritySet(NULL, TASK_PRIORITY_BACKGROUND); // Fall to  our natural priority, as a disinheritance or after load completion.
+		ulTaskNotifyTake(pdTRUE, configTICK_RATE_HZ/10); // Every 0.1 second, check either way.
+
+		// We have received notification but done no work yet, swap waitlists.
+		WaitList *current_wl = this->flushwait[0];
+		this->flushwait[0] = this->flushwait[1]; // New subscriptions will trigger, and wait for, another pass.
+		this->flushwait[1] = current_wl;
+
+		// Step 1: Check the Canary.
+		if (0x1234dead != NVREG32(this->cache, this->eeprom.size)) {
+			this->logtree.log("Canary INVALID.  There has been a buffer overrun in the vicinity of the persistent storage system. EEPROM flushes are PERMANENTLY DISABLED.", LogTree::LOG_CRITICAL);
+			configASSERT(0); // We're done.  We can't trust our cache or comparisons.
+		}
+
+		bool changed = false;
+
+		// We'll flush pages at a time.
+		for (unsigned int addr = 0; addr < this->eeprom.size; addr += this->eeprom.page_size) {
+			if (0 == memcmp(this->cache + addr, this->data + addr, this->eeprom.page_size))
+				continue; // Already clean.
+
+			if (this->eeprom.write(addr, this->data+addr, this->eeprom.page_size) != this->eeprom.page_size) {
+				this->logtree.log(stdsprintf("EEPROM write failed during flush in Persistent Storage service at 0x%04x", addr), LogTree::LOG_ERROR);
+			}
+			else {
+				memcpy(this->cache + addr, this->data + addr, this->eeprom.page_size); // Update cache mirror to match EEPROM
+				changed = true;
+			}
+		}
+		if (changed)
+			this->logtree.log("Changes to persistent storage have been flushed to EEPROM.", LogTree::LOG_INFO);
+		current_wl->wake();
+	}
+}
---
+++ uw-ipmc/services/persistentstorage/PersistentStorage.h
@@ -0,0 +1,69 @@
+/*
+ * PersistentStorage.h
+ *
+ *  Created on: Jan 2, 2018
+ *      Author: jtikalsky
+ */
+
+#ifndef SRC_COMMON_UW_IPMC_SERVICES_PERSISTENTSTORAGE_PERSISTENTSTORAGE_H_
+#define SRC_COMMON_UW_IPMC_SERVICES_PERSISTENTSTORAGE_PERSISTENTSTORAGE_H_
+
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
+#include <IPMC.h>
+#include <drivers/spi_eeprom/SPIEEPROM.h>
+#include <libs/SkyRoad.h>
+#include <libs/ThreadingPrimitives.h>
+
+/**
+ * A persistent storage module backed by a SPI_EEPROM.
+ *
+ * This module will manage background (or foreground) flush of an in-memory
+ * cache of the EEPROM contents, as well as allocation of sections of EEPROM for
+ * specific uses.
+ */
+class PersistentStorage {
+public:
+	PersistentStorage(SPI_EEPROM &eeprom, LogTree &logtree);
+	virtual ~PersistentStorage();
+	bool flush(TickType_t timeout = 0);
+
+	u16 get_section_version(u16 section_id);
+	void set_section_version(u16 section_id, u16 section_version);
+	void *get_section(u16 section_id, u16 section_version, u16 section_size);
+	void delete_section(u16 section_id);
+
+protected:
+	SPI_EEPROM &eeprom; ///< The eeprom backing this storage
+	TaskHandle_t flushtask; ///< The background flush task
+	u8 *cache; ///< The cache of true EEPROM contents for comparison in flush.
+	u8 *data; ///< The data for real use.
+	LogTree &logtree; ///< Log target
+	Event storage_loaded; ///< A threading event indicating the storage is loaded.
+	WaitList *flushwait[2]; ///< A pair of waitlists used to allow for synchronous flush.
+	SemaphoreHandle_t index_mutex; ///< A mutex protecting the section index.
+
+public:
+	void run_flush_thread(); ///< \protected Internal.
+};
+
+/**
+ * This namespace contains ID allocations for sections of persistent storage.
+ *
+ * An ID is a u16 consisting of two one byte fields:
+ *   MSB: Vendor ID
+ *   LSB: Record ID (vendor specific)
+ *
+ * If you write any module or variant of this IPMC which makes use of persistent
+ * storage, please reserve your Vendor ID by making a pull request to the main
+ * repository.
+ */
+namespace PersistentStorageAllocations {
+	/* Vendor 0: RESERVED */
+	const u16 RESERVED_END_OF_INDEX = 0x0000; ///< A marker to internally denote the end of the index.
+	/* Vendor 1: University of Wisconsin */
+	const u16 WISC_SDR_REPOSITORY = 0x0101; ///< The SDR repository.
+};
+
+#endif /* SRC_COMMON_UW_IPMC_SERVICES_PERSISTENTSTORAGE_PERSISTENTSTORAGE_H_ */
