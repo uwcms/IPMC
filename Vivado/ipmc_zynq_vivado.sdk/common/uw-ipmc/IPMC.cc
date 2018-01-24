f68e117f50d0ecaab3604cf70d321b159990f6bc
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-01-24 16:23:54 -0600

    PersistentStorage console commands

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/IPMC.cc
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/IPMC.cc
@@ -79,6 +79,7 @@ void driver_init(bool use_pl) {
 	eeprom_data = new SPI_EEPROM(*ps_spi0, 0, 0x8000, 64);
 	eeprom_mac = new SPI_EEPROM(*ps_spi0, 1, 0x100, 16);
 	persistent_storage = new PersistentStorage(*eeprom_data, LOG["persistent_storage"]);
+	persistent_storage->register_console_commands(console_command_parser, "eeprom.");
 
 	XGpioPs_Config* gpiops_config = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID);
 	configASSERT(XST_SUCCESS == XGpioPs_CfgInitialize(&gpiops, gpiops_config, gpiops_config->BaseAddr));
