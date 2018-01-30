3c581183e7492c7d767c62580f497d0495087c15
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-01-30 12:17:03 -0600

    Define git version hash as non-const only in version.cc
    This is required becuase otherwise it will be implicitly defined as
    static and optimized away by the compiler.  It will be treated as const
    elsewhere becuase its header definition will not change.  The linker
    won't care.

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/version.cc
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/version.cc
@@ -1,6 +1,10 @@
-const long int GIT_SHORT_INT __attribute__((used)) = D_GIT_SHORT_INT; // const ints are static storage by default, we must ensure this one is not eliminated by the compiler.
-const char *GIT_SHORT        = D_GIT_SHORT;
-const char *GIT_LONG         = D_GIT_LONG;
-const char *GIT_DESCRIBE     = D_GIT_DESCRIBE;
-const char *GIT_BRANCH       = D_GIT_BRANCH;
-const char *GIT_STATUS       = D_GIT_STATUS;
+/* const ints are static by default and will be optimized away by the compiler.
+ *
+ * So, this is not const here.  It is in the headers.  The linker won't care.
+ */
+long int GIT_SHORT_INT   = D_GIT_SHORT_INT;
+const char *GIT_SHORT    = D_GIT_SHORT;
+const char *GIT_LONG     = D_GIT_LONG;
+const char *GIT_DESCRIBE = D_GIT_DESCRIBE;
+const char *GIT_BRANCH   = D_GIT_BRANCH;
+const char *GIT_STATUS   = D_GIT_STATUS;
