d43d87b2b4cc6b2d1b5d6fbf6fce5a558eafa632
asvetek <ales.svetek@cern.ch>
2018-02-07 18:48:50 -0600

    Another attempt to properly integrate pyld_pwr_ctrl IP core low level drivers

--- pyld_pwr_ctrl_1.0/drivers/pyld_pwr_ctrl_v1_0/src/pyld_pwr_ctrl.h
+++ pyld_pwr_ctrl_1.0/drivers/pyld_pwr_ctrl_v1_0/src/pyld_pwr_ctrl.h
@@ -21,6 +21,14 @@
 
 #define SW_OFF_MAGIC_WORD      0xC0DEa0FF
 
+#define PE_GROUP_1      0x01
+#define PE_GROUP_2      0x02
+#define PE_GROUP_3      0x04
+#define PE_GROUP_4      0x08
+#define PE_GROUP_5      0x10
+#define PE_GROUP_6      0x20
+#define PE_GROUP_7      0x40
+
 
 /**************************** Type Definitions *****************************/
 
@@ -32,7 +40,7 @@ typedef struct {
     UINTPTR BaseAddress; /* Device base address */
     u32 PECount; /* Number of available Power Enable output pins */
     u32 PGCount; /* Number of available Power Good input pins */
-} Pyld_Pwr_Ctrl_Config_t;
+} Pyld_Pwr_Ctrl_Config;
 
 /**
  * The Pyld_Pwr_Ctrl driver instance data. The user is required to allocate a
@@ -44,37 +52,38 @@ typedef struct {
     u32 IsReady;        /* Device is initialized and ready */
     u32 PECount; /* Number of available Power Enable output pins */
     u32 PGCount; /* Number of available Power Good input pins */
-} Pyld_Pwr_Ctrl_t;
+} Pyld_Pwr_Ctrl;
 
 /**
  * Power Enable Pin Configuration
  **/
 typedef struct {
     u32 group;        /* power enable group  */
-    u32 seq_tmr;      /* power up/down sequence timer config, in microseconds */
-    bool sw_pd_en;    /* enable emergency power down triggered by SW */
-    bool ext_pd_en;   /* enable emergency power down triggered by PL FW */
+    u32 seq_tmr;      /* power up/down sequence timer config, in miliseconds */
+    u8 sw_pd_en;    /* enable emergency power down triggered by SW */
+    u8 ext_pd_en;   /* enable emergency power down triggered by PL FW */
 } PE_cfg_t;
 
-/*
- * API Basic functions implemented in axi_Pyld_Pwr_Ctrl.c
- */
-int Pyld_Pwr_Ctrl_Initialize(Pyld_Pwr_Ctrl_t *InstancePtr, u16 DeviceId);
-Pyld_Pwr_Ctrl_Config_t *Pyld_Pwr_Ctrl_LookupConfig(u16 DeviceId);
 
-int Pyld_Pwr_Ctrl_CfgInitialize(Pyld_Pwr_Ctrl_t *InstancePtr, Pyld_Pwr_Ctrl_Config_t * Config, UINTPTR EffectiveAddr);
+int Pyld_Pwr_Ctrl_Initialize(Pyld_Pwr_Ctrl *InstancePtr, u16 DeviceId);
+Pyld_Pwr_Ctrl_Config *Pyld_Pwr_Ctrl_LookupConfig(u16 DeviceId);
 
-u32 Pyld_Pwr_Ctrl_Get_Core_Ver(Pyld_Pwr_Ctrl_t *InstancePtr);
-u32 Pyld_Pwr_Ctrl_Get_PE_Cnt(Pyld_Pwr_Ctrl_t *InstancePtr);
-u32 Pyld_Pwr_Ctrl_Get_PG_Cnt(Pyld_Pwr_Ctrl_t *InstancePtr);
+int Pyld_Pwr_Ctrl_CfgInitialize(Pyld_Pwr_Ctrl *InstancePtr, Pyld_Pwr_Ctrl_Config * Config, UINTPTR EffectiveAddr);
 
-void Pyld_Pwr_Ctrl_Get_Pin_Cfg(Pyld_Pwr_Ctrl_t *InstancePtr, u32 pin, PE_cfg_t * PE_cfg);
-void Pyld_Pwr_Ctrl_Set_Pin_Cfg(Pyld_Pwr_Ctrl_t *InstancePtr, u32 pin, PE_cfg_t PE_cfg);
+u32 Pyld_Pwr_Ctrl_Get_Core_Ver(Pyld_Pwr_Ctrl *InstancePtr);
+u32 Pyld_Pwr_Ctrl_Get_PE_Cnt(Pyld_Pwr_Ctrl *InstancePtr);
+u32 Pyld_Pwr_Ctrl_Get_PG_Cnt(Pyld_Pwr_Ctrl *InstancePtr);
 
-void Pyld_Pwr_Ctrl_PDown_Force(Pyld_Pwr_Ctrl_t *InstancePtr);
-void Pyld_Pwr_Ctrl_PDown_Release(Pyld_Pwr_Ctrl_t *InstancePtr);
+void Pyld_Pwr_Ctrl_Get_Pin_Cfg(Pyld_Pwr_Ctrl *InstancePtr, u32 pin, PE_cfg_t * PE_cfg);
+void Pyld_Pwr_Ctrl_Set_Pin_Cfg(Pyld_Pwr_Ctrl *InstancePtr, u32 pin, PE_cfg_t PE_cfg);
 
-void Pyld_Pwr_Ctrl_Init_PDown_Seq(Pyld_Pwr_Ctrl_t *InstancePtr, u32 group);
-void Pyld_Pwr_Ctrl_Init_PUp_Seq(Pyld_Pwr_Ctrl_t *InstancePtr, u32 group);
+void Pyld_Pwr_Ctrl_PDown_Force(Pyld_Pwr_Ctrl *InstancePtr);
+void Pyld_Pwr_Ctrl_PDown_Release(Pyld_Pwr_Ctrl *InstancePtr);
+
+void Pyld_Pwr_Ctrl_Init_PDown_Seq(Pyld_Pwr_Ctrl *InstancePtr, u32 group);
+void Pyld_Pwr_Ctrl_Init_PUp_Seq(Pyld_Pwr_Ctrl *InstancePtr, u32 group);
+
+u32 Pyld_Pwr_Ctrl_Get_PE_Status(Pyld_Pwr_Ctrl *InstancePtr);
+u32 Pyld_Pwr_Ctrl_Get_PG_Status(Pyld_Pwr_Ctrl *InstancePtr);
 
 #endif // Pyld_Pwr_Ctrl_H
