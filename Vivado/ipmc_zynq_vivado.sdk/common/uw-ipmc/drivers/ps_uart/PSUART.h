6cbfa49400924492f5690c0a048f4e1d251ee31c
Marcelo Vicente <marcelo.vicente@cern.ch>
2018-11-02 15:12:38 -0500

    Adds NEON compilation flags, adds code to have localized interrupt enable/disables, optimizes RingBuffer and also the UART driver

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/drivers/ps_uart/PSUART.h
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/drivers/ps_uart/PSUART.h
@@ -14,36 +14,41 @@
  * This driver will maintain an input and output buffer of user-specified
  * byte length.
  */
-class PS_UART : public UART, protected InterruptBasedDriver {
+class PS_UART final : public UART, protected InterruptBasedDriver {
 public:
-	PS_UART(u32 DeviceId, u32 IntrId, u32 ibufsize=4096, u32 obufsize=4096, u32 oblocksize=128);
-	virtual size_t read(u8 *buf, size_t len, TickType_t timeout, TickType_t data_timeout=portMAX_DELAY);
-	/// \overload
-	virtual size_t read(char *buf, size_t len, TickType_t timeout, TickType_t data_timeout=portMAX_DELAY) { return this->read(reinterpret_cast<u8*>(buf), len, timeout, data_timeout); };
-	virtual size_t write(const u8 *buf, size_t len, TickType_t timeout);
-	/// \overload
-	virtual size_t write(const char *buf, size_t len, TickType_t timeout) { return this->write(reinterpret_cast<const u8*>(buf), len, timeout); }
-	virtual bool clear();
-	virtual ~PS_UART();
-	virtual void _HandleInterrupt(u32 Event, u32 EventData); ///< \protected Internal.
+	/**
+	 * Instantiate a PS_UART driver.
+	 *
+	 * \note This performs hardware setup (mainly interrupt configuration).
+	 *
+	 * \param DeviceId    The DeviceId, used for XUartPs_LookupConfig(), etc
+	 * \param IntrId      The interrupt ID, for configuring the GIC.
+	 * \param ibufsize    The size of the input buffer to allocate.
+	 * \param obufsize    The size of the output buffer to allocate.
+	 */
+	PS_UART(u32 DeviceId, u32 IntrId, u32 ibufsize = 4096, u32 obufsize = 4096);
+	~PS_UART();
+
+	size_t read(u8 *buf, size_t len, TickType_t timeout, TickType_t data_timeout=portMAX_DELAY);
+	size_t write(const u8 *buf, size_t len, TickType_t timeout);
+	bool clear();
+
+	///! Returns the total number of errors.
+	inline u32 getErrors() { return this->error_count; };
 
     PS_UART(PS_UART const &) = delete;               ///< Class is not assignable.
     void operator=(PS_UART const &x) = delete;       ///< Class is not copyable.
 protected:
-	u32 error_mask;        ///< Error mask containing accumulated errors from recent operations. TODO: allow read.
-	XUartPs UartInst;      ///< The XUartPs handle of the driven device.
-	RingBuffer<u8> inbuf;  ///< The input buffer.
-	RingBuffer<u8> outbuf; ///< The output buffer.
-	WaitList readwait;     ///< A waitlist for blocking read operations.
-	WaitList writewait;    ///< A waitlist for blocking write operations.
-	u32 oblocksize;        ///< The maximum block size for output operations.  Relevant to wait times when the queue is full.
-	void _InterruptHandler();
+	volatile u32 error_count;       ///< Error counter containing accumulated errors from recent operations.
+	XUartPs UartInst;      			///< The XUartPs handle of the driven device.
+	RingBuffer<u8> inbuf;  			///< The input buffer.
+	RingBuffer<u8> outbuf; 			///< The output buffer.
+	WaitList readwait;     			///< A waitlist for blocking read operations.
+	WaitList writewait;    			///< A waitlist for blocking write operations.
+	void _InterruptHandler();		///< Internal interrupt handler for UART IRQ.
 
-	u32 XUartPs_Recv(XUartPs *InstancePtr);
-	u32 XUartPs_Send(XUartPs *InstancePtr);
-
-	u32 XUartPs_ReceiveBuffer(XUartPs *InstancePtr);
-	u32 XUartPs_SendBuffer(XUartPs *InstancePtr);
+	void recv();					///< Internally called to receive to the ring buffer.
+	void send();					///< Internally called to send from the ring buffer.
 };
 
 #endif /* UW_IPMC_DRIVERS_PS_UART_PSUART_H_ */
