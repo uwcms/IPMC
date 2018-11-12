uwcms/IPMC

commit 1b5a3efe9c81023020e5764e50379987e65b7351

SDR Parsers & Classes, first pass (sensors only)

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/IPMIFormats.cpp
@@ -7,6 +7,24 @@
 
 #include <services/ipmi/IPMIFormats.h>
 #include <IPMC.h> // stdsprintf()
+#include <iterator>
+
+/**
+ * Perform basic validation on the provided IPMI Type/Length field, and return
+ * the length in bytes of the full raw field.
+ *
+ * @param data The Type/Length field
+ * @return 0 if invalid, else the length in bytes, including the header
+ */
+unsigned ipmi_type_length_field_get_length(const std::vector<uint8_t> &data) {
+	if (data.empty())
+		return 0;
+	//const uint8_t field_type = data[0] >> 6;
+	const unsigned field_length = data[0] & 0x3f;
+	if (1+field_length > data.size())
+		return 0;
+	return 1+field_length;
+}
 
 /**
  * Unpack and render a field specified in Type/Length byte format, as
@@ -15,15 +33,14 @@
  * @param data The field data including the type/length code byte.
  * @return The rendered data in human readable format.
  */
-std::string render_ipmi_type_length_field(std::vector<uint8_t> &data) {
+std::string render_ipmi_type_length_field(const std::vector<uint8_t> &data) {
 	if (data.empty())
 		return "<Invalid (Short) Type/Length Field>";
 	const uint8_t field_type = data[0] >> 6;
-	const uint8_t field_length = data[0] & 0x3f;
-	data.erase(data.begin());
+	const unsigned field_length = data[0] & 0x3f;
 	if (field_length == 0)
 		return "<NULL>";
-	if (field_length < data.size())
+	if (1+field_length > data.size())
 		return "<Invalid (Short) Type/Length Field>";
 	if (field_type == 0) {
 		// Binary or Unspecified.
@@ -31,7 +48,7 @@ std::string render_ipmi_type_length_field(std::vector<uint8_t> &data) {
 		for (std::vector<uint8_t>::size_type i = 0; i < field_length; ++i) {
 			if (out.size())
 				out += " ";
-			out += stdsprintf("0x%02hhx", data[i]);
+			out += stdsprintf("0x%02hhx", data[1+i]);
 		}
 		return out;
 	}
@@ -40,8 +57,8 @@ std::string render_ipmi_type_length_field(std::vector<uint8_t> &data) {
 		std::string out;
 		for (std::vector<uint8_t>::size_type i = 0; i < field_length; ++i) {
 			static const char *bcd_table = "01234567889 -.???";
-			out += bcd_table[data[i] >> 4];
-			out += bcd_table[data[i] & 0x0f];
+			out += bcd_table[data[1+i] >> 4];
+			out += bcd_table[data[1+i] & 0x0f];
 		}
 		return out;
 	}
@@ -54,7 +71,7 @@ std::string render_ipmi_type_length_field(std::vector<uint8_t> &data) {
 		for (std::vector<uint8_t>::size_type i = 0; i*6 < count6b; ++i) {
 			char c;
 			// Calculate the byte offset in which this char starts.
-			std::vector<uint8_t>::size_type offset = (i*6)/8;
+			std::vector<uint8_t>::size_type offset = 1 /* header*/ + ((i*6)/8);
 			switch (i % 4) {
 			case 0:
 				/* The first char in a cycle is zero aligned.
@@ -92,11 +109,11 @@ std::string render_ipmi_type_length_field(std::vector<uint8_t> &data) {
 	}
 	else if (field_type == 3) {
 		// Raw ASCII or Unicode.
-		return std::string(data.begin(), data.begin()+field_length);
+		return std::string(std::next(data.begin()), std::next(data.begin(),field_length));
 	}
 	else {
 		// We have already iterated over values [0,1,2,3] of a 2 bit field.
 		configASSERT(0);
-		__builtin_unreachable();
+		return "";
 	}
 }

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/IPMIFormats.h
@@ -12,6 +12,7 @@
 #include <string>
 #include <stddef.h>
 
-std::string render_ipmi_type_length_field(std::vector<uint8_t> &data);
+unsigned ipmi_type_length_field_get_length(const std::vector<uint8_t> &data);
+std::string render_ipmi_type_length_field(const std::vector<uint8_t> &data);
 
 #endif /* SRC_COMMON_UW_IPMC_SERVICES_IPMI_IPMIFORMATS_H_ */

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord.cpp
@@ -1,0 +1,76 @@
+/*
+ * SensorDataRecord.cpp
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#include <services/ipmi/sdr/SensorDataRecord.h>
+#include <IPMC.h>
+
+/**
+ * Validate the current Sensor Data Record.
+ *
+ * If this function returns true, it is safe to read all relevant fields from
+ * the record.  It only tests if the data is well-formed for the
+ * SensorDataRecord subclass it is being interpreted by.  It may not be valid
+ * with any other parser.
+ *
+ * \warning Do not call any accessors on a record that is not valid.
+ *
+ * @return true if the record is well-formed, else false
+ */
+bool SensorDataRecord::validate() {
+	if (this->sdr_data.size() < 4)
+		return false;
+	if (this->sdr_data.size() != (4U + this->sdr_data[3]))
+		return false;
+	return true;
+}
+
+/**
+ * Return a shared_ptr to a the appropriate generic SensorDataRecord subclass
+ * for parsing and managing this record.
+ *
+ * \note This will return a NULL shared_ptr if the record cannot be handled by
+ *       an appropriate subclass.
+ *
+ * @return A shared_ptr to a subclass if possible, false otherwise.
+ */
+std::shared_ptr<SensorDataRecord> SensorDataRecord::interpret() {
+	if (!this->validate())
+		return NULL;
+	switch (this->get_record_type()) {
+	default:
+		/* If interpretation fails, return NULL, rather than a copy of *this, so
+		 * that the user knows not to call get_record_key().
+		 */
+		return NULL;
+	}
+}
+
+uint16_t SensorDataRecord::get_record_id() {
+	configASSERT(this->sdr_data.size() >= 2);
+	return (this->sdr_data[0] << 8) | this->sdr_data[1];
+}
+
+void SensorDataRecord::set_record_id(uint16_t record_id) {
+	configASSERT(this->sdr_data.size() >= 2);
+	this->sdr_data[0] = record_id >> 8;
+	this->sdr_data[1] = record_id & 0xff;
+}
+
+uint8_t SensorDataRecord::get_record_version() {
+	configASSERT(this->sdr_data.size() >= 3);
+	return this->sdr_data[2];
+}
+
+uint8_t SensorDataRecord::get_record_type() {
+	configASSERT(this->sdr_data.size() >= 4);
+	return this->sdr_data[3];
+}
+
+std::vector<uint8_t> SensorDataRecord::get_record_key() {
+	configASSERT(0); // Supported only by derived classes.
+	return std::vector<uint8_t>();
+}

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord.h
@@ -1,0 +1,57 @@
+/*
+ * SensorDataRecord.h
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#ifndef SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD_H_
+#define SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD_H_
+
+#include <vector>
+#include <stdint.h>
+#include <memory>
+
+/**
+ * A minimal class implementing the most basic level of SDR(ecord) support,
+ * intended as an interface superclass for all SDR(ecord) objects.
+ *
+ * \warning Do not call any accessors on a record that does not validate().
+ */
+class SensorDataRecord {
+public:
+	std::vector<uint8_t> sdr_data; ///< Sensor Data Record bytes
+
+	/**
+	 * Instantiate a SensorDataRecord object with the provided raw record.
+	 * @param sdr_data The raw SDR record bytes
+	 */
+	SensorDataRecord(const std::vector<uint8_t> &sdr_data) : sdr_data(sdr_data) { };
+	virtual ~SensorDataRecord() { };
+
+	virtual bool validate();
+
+	std::shared_ptr<SensorDataRecord> interpret(); // TODO: Review.
+
+	/**
+	 * SDR Data Accessors
+	 *
+	 * \warning Do not call any accessors on a record that does not validate().
+	 */
+	///@{
+	virtual uint16_t get_record_id();
+	virtual void set_record_id(uint16_t record_id);
+	virtual uint8_t get_record_version();
+	virtual uint8_t get_record_type();
+
+	/**
+	 * Get the record key bytes for this SensorDataRecord subclass.
+	 *
+	 * \warning Do not call this on an un-interpreted SensorDataRecord (i.e. non-subclass).
+	 * @return The record key bytes.
+	 */
+	virtual std::vector<uint8_t> get_record_key();
+	///@}
+};
+
+#endif /* SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD_H_ */

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord01.cpp
@@ -1,0 +1,111 @@
+/*
+ * SensorDataRecord01.cpp
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#include <services/ipmi/sdr/SensorDataRecord01.h>
+#include <IPMC.h>
+
+/// Create a bitmask with the lower `nbits` bits set.
+#define LOWBITS(nbits) (0xff >> (8-(nbits)))
+
+/// Define a `type` type SDR_FIELD from byte `byte`[a:b].
+#define SDR_FIELD(name, type, byte, a, b) \
+	type SensorDataRecord01::name() { \
+		configASSERT(this->validate()); \
+		return static_cast<type>((this->sdr_data[byte] >> (b)) & LOWBITS((a)-(b)+1)); \
+	} \
+	void SensorDataRecord01::name(type val) { \
+		configASSERT((static_cast<uint8_t>(val) & LOWBITS((a)-(b)+1)) == static_cast<uint8_t>(val)); \
+		configASSERT(this->validate()); \
+		this->sdr_data[byte] &= ~(LOWBITS((a)-(b)+1)<<(b)); /* Erase old value */ \
+		this->sdr_data[byte] |= static_cast<uint8_t>(val)<<(b); /* Set new value */ \
+	}
+
+SDR_FIELD(linearization, enum SensorDataRecord01::Linearization, 23, 7, 0)
+
+uint16_t SensorDataRecord01::conversion_m() {
+	configASSERT(this->validate());
+	return this->sdr_data[24] | ((this->sdr_data[25]&0xc0)<<2);
+}
+void SensorDataRecord01::conversion_m(uint16_t val) {
+	configASSERT((val & 0x3ff) == val);
+	configASSERT(this->validate());
+	this->sdr_data[24] = val & 0xff;
+	this->sdr_data[25] &= ~0xc0;
+	this->sdr_data[25] |= val >> 8;
+}
+
+SDR_FIELD(conversion_m_tolerance, uint8_t, 25, 5, 0) // Unit: +/- half raw counts
+
+uint16_t SensorDataRecord01::conversion_b() {
+	configASSERT(this->validate());
+	return this->sdr_data[26] | ((this->sdr_data[27]&0xc0)<<2);
+}
+void SensorDataRecord01::conversion_b(uint16_t val) {
+	configASSERT((val & 0x3ff) == val);
+	configASSERT(this->validate());
+	this->sdr_data[26] = val & 0xff;
+	this->sdr_data[27] &= ~0xc0;
+	this->sdr_data[27] |= val >> 8;
+}
+
+uint16_t SensorDataRecord01::conversion_b_accuracy() {
+	configASSERT(this->validate());
+	return (this->sdr_data[27] & 0x3f) | ((this->sdr_data[28]&0xf0)<<2);
+}
+void SensorDataRecord01::conversion_b_accuracy(uint16_t val) {
+	configASSERT((val & 0x3ff) == val);
+	configASSERT(this->validate());
+	this->sdr_data[27] &= ~0x3f;
+	this->sdr_data[27] |= val & 0x3f;
+	this->sdr_data[28] &= ~0xf0;
+	this->sdr_data[28] |= (val >> 2) & 0xf0;
+}
+
+SDR_FIELD(conversion_b_accuracy_exp, uint8_t, 28, 3, 2)
+
+SDR_FIELD(sensor_direction, enum SensorDataRecordReadableSensor::Direction, 28, 1, 0)
+
+int8_t SensorDataRecord01::r_exp() {
+	configASSERT(this->validate());
+	uint8_t val = this->sdr_data[29] >> 4;
+	if (val & 0x08)
+		val |= 0xf0; // Extend the 1 sign to 8 bits. (2s complement)
+	return *reinterpret_cast<int8_t*>(&val);
+}
+void SensorDataRecord01::r_exp(int8_t val) {
+	uint8_t uval = *reinterpret_cast<uint8_t*>(&val);
+	configASSERT((uval&0xf0) == 0 || (uval&0xf0) == 0xf0);
+	configASSERT(this->validate());
+	this->sdr_data[29] = (uval<<4) | (this->sdr_data[29] & 0x0f);
+}
+
+int8_t SensorDataRecord01::b_exp() {
+	configASSERT(this->validate());
+	uint8_t val = this->sdr_data[29] & 0x0f;
+	if (val & 0x08)
+		val |= 0xf0; // Extend the 1 sign to 8 bits. (2s complement)
+	return *reinterpret_cast<int8_t*>(&val);
+}
+void SensorDataRecord01::b_exp(int8_t val) {
+	uint8_t uval = *reinterpret_cast<uint8_t*>(&val);
+	configASSERT((uval&0xf0) == 0 || (uval&0xf0) == 0xf0);
+	configASSERT(this->validate());
+	this->sdr_data[29] = (this->sdr_data[29] & 0xf0) | uval;
+}
+
+SDR_FIELD(normal_min_specified, bool, 30, 2, 2)
+SDR_FIELD(normal_max_specified, bool, 30, 1, 1)
+SDR_FIELD(nominal_reading_specified, bool, 30, 0, 0)
+
+SDR_FIELD(nominal_reading_rawvalue, uint8_t, 31, 7, 0)
+SDR_FIELD(normal_max_rawvalue, uint8_t, 32, 7, 0)
+SDR_FIELD(normal_min_rawvalue, uint8_t, 33, 7, 0)
+
+SDR_FIELD(hysteresis_high, uint8_t, 42, 7, 0)
+SDR_FIELD(hysteresis_low, uint8_t, 43, 7, 0)
+
+SDR_FIELD(oem, uint8_t, 46, 7, 0)

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord01.h
@@ -1,0 +1,97 @@
+/*
+ * SensorDataRecord01.h
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#ifndef SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD01_H_
+#define SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD01_H_
+
+#include <services/ipmi/sdr/SensorDataRecordReadableSensor.h>
+#include <stdint.h>
+#include <vector>
+
+/**
+ * A SensorDataRecord representing and parsing a Type 01 SDR.
+ */
+class SensorDataRecord01 : public SensorDataRecordReadableSensor {
+public:
+	/// Instantiate a Type 01 SensorDataRecord
+	SensorDataRecord01(const std::vector<uint8_t> &sdr_data) : SensorDataRecordSensor(sdr_data), SensorDataRecordReadableSensor(sdr_data) { };
+	virtual ~SensorDataRecord01() { };
+
+	/**
+	 * SDR Data Accessors
+	 *
+	 * \warning Do not call any accessors on a record that does not validate().
+	 */
+	///@{
+#define SDR_FIELD(name, type, byte, a, b) \
+	virtual type name(); \
+	virtual void name(type val);
+
+	enum Linearization {
+		LIN_LINEAR = 0,
+		LIN_LN,
+		LIN_LOG10,
+		LIN_LOG2,
+		LIN_E,
+		LIN_EXP10,
+		LIN_EXP2,
+		LIN_1DIVX,
+		LIN_SQRX,
+		LIN_CUBEX,
+		LIN_SQRTX,
+		LIN_CUBERTX,
+		LIN_NONLINEAR       = 0x70,
+		LIN_NONLINEAR_OEM_1 = 0x71,
+		LIN_NONLINEAR_OEM_2 = 0x72,
+		LIN_NONLINEAR_OEM_3 = 0x73,
+		LIN_NONLINEAR_OEM_4 = 0x74,
+		LIN_NONLINEAR_OEM_5 = 0x75,
+		LIN_NONLINEAR_OEM_6 = 0x76,
+		LIN_NONLINEAR_OEM_7 = 0x77,
+		LIN_NONLINEAR_OEM_8 = 0x78,
+		LIN_NONLINEAR_OEM_9 = 0x79,
+		LIN_NONLINEAR_OEM_A = 0x7A,
+		LIN_NONLINEAR_OEM_B = 0x7B,
+		LIN_NONLINEAR_OEM_C = 0x7C,
+		LIN_NONLINEAR_OEM_D = 0x7D,
+		LIN_NONLINEAR_OEM_E = 0x7E,
+		LIN_NONLINEAR_OEM_F = 0x7F,
+	};
+	SDR_FIELD(linearization, enum Linearization, 23, 7, 0)
+
+	SDR_FIELD(conversion_m, uint16_t, 24 AND 25, 7, 0)
+	SDR_FIELD(conversion_m_tolerance, uint8_t, 25, 5, 0) // Unit: +/- half raw counts
+
+	SDR_FIELD(conversion_b, uint16_t, 26 AND 27, 7, 0)
+	SDR_FIELD(conversion_b_accuracy, uint16_t, 27 AND 28, 7, 0)
+	SDR_FIELD(conversion_b_accuracy_exp, uint8_t, 28, 3, 2)
+
+	SDR_FIELD(sensor_direction, enum SensorDataRecordReadableSensor::Direction, 28, 1, 0)
+
+	SDR_FIELD(r_exp, int8_t, 29, 7, 4)
+	SDR_FIELD(b_exp, int8_t, 29, 3, 0)
+
+	SDR_FIELD(normal_min_specified, bool, 30, 2, 2)
+	SDR_FIELD(normal_max_specified, bool, 30, 1, 1)
+	SDR_FIELD(nominal_reading_specified, bool, 30, 0, 0)
+
+	SDR_FIELD(nominal_reading_rawvalue, uint8_t, 31, 7, 0)
+	SDR_FIELD(normal_max_rawvalue, uint8_t, 32, 7, 0)
+	SDR_FIELD(normal_min_rawvalue, uint8_t, 33, 7, 0)
+
+	SDR_FIELD(hysteresis_high, uint8_t, 42, 7, 0)
+	SDR_FIELD(hysteresis_low, uint8_t, 43, 7, 0)
+
+	SDR_FIELD(oem, uint8_t, 46, 7, 0)
+
+#undef SDR_FIELD
+	virtual uint8_t _get_id_string_offset() { return 47; };
+	///@}
+
+};
+
+#endif /* SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SENSORDATARECORD01_H_ */

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord02.cpp
@@ -1,0 +1,37 @@
+/*
+ * SensorDataRecord02.cpp
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#include <services/ipmi/sdr/SensorDataRecord02.h>
+#include <IPMC.h>
+
+/// Create a bitmask with the lower `nbits` bits set.
+#define LOWBITS(nbits) (0xff >> (8-(nbits)))
+
+/// Define a `type` type SDR_FIELD from byte `byte`[a:b].
+#define SDR_FIELD(name, type, byte, a, b) \
+	type SensorDataRecord02::name() { \
+		configASSERT(this->validate()); \
+		return static_cast<type>((this->sdr_data[byte] >> (b)) & LOWBITS((a)-(b)+1)); \
+	} \
+	void SensorDataRecord02::name(type val) { \
+		configASSERT((static_cast<uint8_t>(val) & LOWBITS((a)-(b)+1)) == static_cast<uint8_t>(val)); \
+		configASSERT(this->validate()); \
+		this->sdr_data[byte] &= ~(LOWBITS((a)-(b)+1)<<(b)); /* Erase old value */ \
+		this->sdr_data[byte] |= static_cast<uint8_t>(val)<<(b); /* Set new value */ \
+	}
+
+SDR_FIELD(sensor_direction, enum SensorDataRecordReadableSensor::Direction, 23, 7, 6)
+
+SDR_FIELD(id_string_instance_modifier_type, enum SensorDataRecordSharedSensor::IDStringInstanceModifierType, 23, 5, 4)
+SDR_FIELD(share_count, uint8_t, 23, 3, 0)
+SDR_FIELD(entity_instance_sharing, uint8_t, 24, 7, 7)
+SDR_FIELD(id_string_instance_modifier_offset, uint8_t, 23, 6, 0)
+
+SDR_FIELD(hysteresis_high, uint8_t, 25, 7, 0)
+SDR_FIELD(hysteresis_low, uint8_t, 26, 7, 0)
+
+SDR_FIELD(oem, uint8_t, 30, 7, 0)

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sdr/SensorDataRecord02.h
@@ -1,0 +1,53 @@
+/*
+ * SensorDataRecord02.h
+ *
+ *  Created on: Aug 3, 2018
+ *      Author: jtikalsky
+ */
+
+#ifndef SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SensorDataRecord02_H_
+#define SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SensorDataRecord02_H_
+
+#include <services/ipmi/sdr/SensorDataRecordReadableSensor.h>
+#include <services/ipmi/sdr/SensorDataRecordSharedSensor.h>
+#include <stdint.h>
+#include <vector>
+
+/**
+ * A SensorDataRecord representing and parsing a Type 02 SDR.
+ */
+class SensorDataRecord02 : public SensorDataRecordReadableSensor, SensorDataRecordSharedSensor {
+public:
+	/// Instantiate a Type 02 SensorDataRecord
+	SensorDataRecord02(const std::vector<uint8_t> &sdr_data) : SensorDataRecordSensor(sdr_data), SensorDataRecordReadableSensor(sdr_data), SensorDataRecordSharedSensor(sdr_data) { };
+	virtual ~SensorDataRecord02() { };
+
+	/**
+	 * SDR Data Accessors
+	 *
+	 * \warning Do not call any accessors on a record that does not validate().
+	 */
+	///@{
+#define SDR_FIELD(name, type, byte, a, b) \
+	virtual type name(); \
+	virtual void name(type val);
+
+	SDR_FIELD(sensor_direction, enum SensorDataRecordReadableSensor::Direction, 23, 7, 6)
+
+	SDR_FIELD(id_string_instance_modifier_type, enum SensorDataRecordSharedSensor::IDStringInstanceModifierType, 23, 5, 4)
+	SDR_FIELD(share_count, uint8_t, 23, 3, 0)
+	SDR_FIELD(entity_instance_sharing, uint8_t, 24, 7, 7)
+	SDR_FIELD(id_string_instance_modifier_offset, uint8_t, 23, 6, 0)
+
+	SDR_FIELD(hysteresis_high, uint8_t, 25, 7, 0)
+	SDR_FIELD(hysteresis_low, uint8_t, 26, 7, 0)
+
+	SDR_FIELD(oem, uint8_t, 30, 7, 0)
+
+#undef SDR_FIELD
+	virtual uint8_t _get_id_string_offset() { return 31; };
+	///@}
+
+};
+
+#endif /* SRC_COMMON_UW_IPMC_SERVICES_IPMI_SDR_SensorDataRecord02_H_ */

commit 7b9274a280cfee1d2827ec59af5a8016db2e2305

IPMIFormats: Fix offset handling in the type/length string parser broken in 1b5a3efe
When this was converted from a copy to a const reference, I changed the
Packed 6-bit ASCII's starting offset to 1, but this results in incorrect
parsing becuase it offsets the state machine by one.  I also failed to
correctly update the Raw ASCII or Unicode handler.  Both of these
issues are now corrected.

Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/IPMIFormats.cpp
@@ -71,35 +71,35 @@ std::string render_ipmi_type_length_field(const std::vector<uint8_t> &data) {
 		for (std::vector<uint8_t>::size_type i = 0; i*6 < count6b; ++i) {
 			char c;
 			// Calculate the byte offset in which this char starts.
-			std::vector<uint8_t>::size_type offset = 1 /* header*/ + ((i*6)/8);
+			std::vector<uint8_t>::size_type offset = ((i*6)/8);
 			switch (i % 4) {
 			case 0:
 				/* The first char in a cycle is zero aligned.
 				 *
 				 * Nothing fancy here.
 				 */
-				c = data[offset] & 0x3f;
+				c = data[1+offset] & 0x3f;
 				break;
 			case 1:
 				/* The second char in a cycle is offset by 6.
 				 *
 				 * Take the last two of that, and the first two of the next.
 				 */
-				c = (data[offset+1] & 0x0f) | (data[offset] >> 6);
+				c = (data[1+offset+1] & 0x0f) | (data[1+offset] >> 6);
 				break;
 			case 2:
 				/* The third char in a cycle is offset by 4.
 				 *
 				 * Take the last four of that, and the first two of the next.
 				 */
-				c = (data[offset+1] & 0x03) | (data[offset] >> 4);
+				c = (data[1+offset+1] & 0x03) | (data[1+offset] >> 4);
 				break;
 			case 3:
 				/* The fourth char in a cycle is offset by 2.
 				 *
 				 * Take the last 6 of that.
 				 */
-				c = data[offset] >> 2;
+				c = data[1+offset] >> 2;
 				break;
 			}
 			// Now convert 6b to 8b ASCII.
@@ -109,7 +109,7 @@ std::string render_ipmi_type_length_field(const std::vector<uint8_t> &data) {
 	}
 	else if (field_type == 3) {
 		// Raw ASCII or Unicode.
-		return std::string(std::next(data.begin()), std::next(data.begin(),field_length));
+		return std::string(std::next(data.begin()), std::next(data.begin(),1+field_length));
 	}
 	else {
 		// We have already iterated over values [0,1,2,3] of a 2 bit field.