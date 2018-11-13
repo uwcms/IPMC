107fc11492a9819597bef56f7a3ab37de306c0d9
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-11-13 15:12:52 -0600

    Default to M1 startup state

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sensor/HotswapSensor.cpp
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/sensor/HotswapSensor.cpp
@@ -6,6 +6,18 @@
  */
 
 #include <services/ipmi/sensor/HotswapSensor.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+
+HotswapSensor::HotswapSensor(const std::vector<uint8_t> &sdr_key, LogTree &log)
+	: Sensor(sdr_key, log), mstate(1), previous_mstate(0),
+	  last_transition_reason(TRANS_NORMAL) {
+	configASSERT(this->mutex = xSemaphoreCreateMutex());
+}
+
+HotswapSensor::~HotswapSensor() {
+	vSemaphoreDelete(this->mutex);
+}
 
 /**
  * Update the current M-state and send the appropriate events.
@@ -15,10 +27,14 @@
 void HotswapSensor::transition(uint8_t new_state, enum StateTransitionReason reason) {
 	configASSERT(new_state < 8); // Check in range.
 	std::vector<uint8_t> data;
+	xSemaphoreTake(this->mutex, portMAX_DELAY);
 	data.push_back(0xA|new_state);
 	data.push_back((static_cast<uint8_t>(reason)<<4)|this->mstate);
 	data.push_back(0 /* FRU Device ID */);
+	this->previous_mstate = this->mstate;
 	this->mstate = new_state;
+	this->last_transition_reason = reason;
+	xSemaphoreGive(this->mutex);
 	this->send_event(Sensor::EVENT_ASSERTION, data);
 }
 
@@ -33,8 +49,15 @@ std::vector<uint8_t> HotswapSensor::get_sensor_reading() {
 }
 
 void HotswapSensor::rearm() {
-	/* It is unclear what reason code I am expected to send in the event of a
-	 * rearm of the hotswap sensor, so I will send "Unknown Reason".
+	/* It is unclear what previous state or reason code I am expected to send in
+	 * the event of a rearm of the hotswap sensor, but I presume we should
+	 * resend the last event.
 	 */
-	this->transition(this->mstate, HotswapSensor::TRANS_REASON_UNKNOWN);
+	std::vector<uint8_t> data;
+	xSemaphoreTake(this->mutex, portMAX_DELAY);
+	data.push_back(0xA|this->mstate);
+	data.push_back((static_cast<uint8_t>(this->last_transition_reason)<<4)|this->previous_mstate);
+	data.push_back(0 /* FRU Device ID */);
+	xSemaphoreGive(this->mutex);
+	this->send_event(Sensor::EVENT_ASSERTION, data);
 }
