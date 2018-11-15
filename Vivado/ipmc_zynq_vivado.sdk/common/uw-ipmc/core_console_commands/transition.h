bf4bbb247ab3df7403204b27b2d05fe8544376c1
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-11-15 10:06:15 -0600

    Move core console commands to separate files.

---
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/core_console_commands/transition.h
@@ -0,0 +1,24 @@
+namespace {
+
+/// A debugging command to transition between M-states.
+class ConsoleCommand_transition : public CommandParser::Command {
+public:
+	virtual std::string get_helptext(const std::string &command) const {
+		return stdsprintf(
+				"%s $new_mstate $reason\n\n"
+				"Transitions to the specified M-state.\n", command.c_str());
+	}
+
+	virtual void execute(std::shared_ptr<ConsoleSvc> console, const CommandParser::CommandParameters &parameters) {
+		CommandParser::CommandParameters::xint8_t mstate, reason;
+		if (!parameters.parse_parameters(1, true, &mstate, &reason)) {
+			console->write("Invalid arguments, see help.\n");
+			return;
+		}
+		std::shared_ptr<HotswapSensor> hotswap = std::dynamic_pointer_cast<HotswapSensor>(ipmc_sensors.find_by_name("Hotswap"));
+		configASSERT(hotswap);
+		hotswap->transition(mstate, static_cast<HotswapSensor::StateTransitionReason>(static_cast<uint8_t>(reason)));
+	}
+};
+
+}
