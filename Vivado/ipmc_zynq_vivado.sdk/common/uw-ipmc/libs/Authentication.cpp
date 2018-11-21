83f741a95e4adc46a5d77944cd287d644ca55b47
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-11-21 12:01:04 -0600

    Don't do sha_256 calculations in a critical section.

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/libs/Authentication.cpp
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/libs/Authentication.cpp
@@ -9,6 +9,7 @@
 
 // TODO: Maybe have this in a configuration function?
 #include "services/persistentstorage/PersistentStorage.h"
+#include <libs/ThreadingPrimitives.h>
 extern PersistentStorage *persistent_storage;
 
 using namespace Auth;
@@ -64,10 +65,12 @@ void Auth::ChangeCredentials(const std::string &user, const std::string &pass) {
 	HashPair *nvHashes = (HashPair*)persistent_storage->get_section(PersistentStorageAllocations::WISC_NETWORK_AUTH, 1, sizeof(HashPair));
 
 	// Generate the hash for provided username and password
-	taskENTER_CRITICAL();
-	sha_256((const uint8_t*)user.data(), user.size(), nvHashes->user);
-	sha_256((const uint8_t*)pass.data(), pass.size(), nvHashes->pass);
-	taskEXIT_CRITICAL();
+	HashPair new_hashes;
+	sha_256((const uint8_t*)user.data(), user.size(), new_hashes.user);
+	sha_256((const uint8_t*)pass.data(), pass.size(), new_hashes.pass);
+	CriticalGuard critical(true);
+	memcpy(nvHashes, &new_hashes, sizeof(HashPair));
+	critical.release();
 	persistent_storage->flush(nvHashes, sizeof(HashPair));
 }
 
@@ -75,11 +78,11 @@ void Auth::ResetCredentials() {
 	HashPair *nvHashes = (HashPair*)persistent_storage->get_section(PersistentStorageAllocations::WISC_NETWORK_AUTH, 1, sizeof(HashPair));
 
 	// Default password and user
-	taskENTER_CRITICAL();
 	const char* default_user = "\x0c\x7a\x0a\xbd\x06\x6e\xd5\x36\xc1\x05\xcf\xaf\x4e\x55\x14\xf5\x86\x65\x07\x9f\x5a\x2a\x52\x12\xea\x32\x01\x90\xd0\xbc\xb6\xd2";
 	const char* default_pass = "\x0c\x7a\x0a\xbd\x06\x6e\xd5\x36\xc1\x05\xcf\xaf\x4e\x55\x14\xf5\x86\x65\x07\x9f\x5a\x2a\x52\x12\xea\x32\x01\x90\xd0\xbc\xb6\xd2";
+	CriticalGuard critical(true);
 	memcpy(nvHashes->user, default_user, SHA_VALBYTES);
 	memcpy(nvHashes->pass, default_pass, SHA_VALBYTES);
-	taskEXIT_CRITICAL();
+	critical.release();
 	persistent_storage->flush(nvHashes, sizeof(HashPair));
 }
