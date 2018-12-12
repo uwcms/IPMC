1c2e4a773a645bf3babe185300f947e64a314a1b
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-12-12 14:41:39 -0600

    Implemented SDR access

--- Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/commands/SensorEvent.cc
+++ Vivado/ipmc_zynq_vivado.sdk/common/uw-ipmc/services/ipmi/commands/SensorEvent.cc
@@ -1,8 +1,16 @@
 #include <services/ipmi/IPMI.h>
 #include <services/ipmi/ipmbsvc/IPMBSvc.h>
 #include <services/ipmi/sensor/SensorSet.h>
+#include <services/ipmi/sdr/SensorDataRepository.h>
+#include <services/ipmi/sdr/SensorDataRecordSensor.h>
 #include <IPMC.h>
 #include "IPMICmd_Index.h"
+
+#define RETURN_ERROR(ipmb, message, completion_code) \
+	do { \
+		ipmb.send(message.prepare_reply({completion_code})); \
+		return; \
+	} while (0)
 
 // Event Commands
 
@@ -95,26 +103,84 @@ IPMICMD_INDEX_REGISTER(PET_Acknowledge);
 
 // Sensor Device Commands
 
-#if 0 // Unimplemented.
 static void ipmicmd_Get_Device_SDR_Info(IPMBSvc &ipmb, const IPMI_MSG &message) {
-	// Unimplemented.
+	uint8_t parameter = 0;
+	if (message.data_len == 1)
+		parameter = message.data[1];
+	else if (message.data_len > 1)
+		RETURN_ERROR(ipmb, message, IPMI::Completion::Request_Data_Length_Invalid);
+
+	// Count the sensors on each LUN.
+	uint16_t lun_sensor_count[4] = {0, 0, 0, 0};
+	std::vector< std::shared_ptr<const SensorDataRecord> > dsdrr(device_sdr_repo);
+	for (auto it = dsdrr.begin(), eit = dsdrr.end(); it != eit; ++it) {
+		std::shared_ptr<const SensorDataRecordSensor> sdrs = std::dynamic_pointer_cast<const SensorDataRecordSensor>(*it);
+		if (!sdrs)
+			continue;
+		if (sdrs->sensor_owner_lun() < 4)
+			++lun_sensor_count[sdrs->sensor_owner_lun()];
+	}
+
+	std::shared_ptr<IPMI_MSG> reply = message.prepare_reply({IPMI::Completion::Success, 0, 0, 0, 0, 0, 0});
+	if (parameter & 1) {
+		// Return the number of records in the Device SDR Repository
+		reply->data[1] = device_sdr_repo.size();
+	}
+	else {
+		// Return the number of sensors (specifically) on the current LUN (see request message)
+		reply->data[1] = lun_sensor_count[message.rsLUN] > 255 ? 255 : lun_sensor_count[message.rsLUN];
+	}
+
+	/* Dynamic Sensor Population.  I mean, I don't want to guarantee our
+	 * Device SDR Repo *won't* change, even though it /probably/ won't.
+	 */
+	reply->data[2] |= 0x80;
+
+	for (int i = 0; i < 4; ++i)
+		if (lun_sensor_count[i])
+			reply->data[2] |= (1 << i); // Sensors exist on this LUN.
+
+	time_t update_ts = device_sdr_repo.last_update_timestamp();
+	for (int i = 0; i < 4; ++i)
+		reply->data[3+i] = (update_ts >> (8*i)) & 0xff;
+	ipmb.send(reply);
 }
 IPMICMD_INDEX_REGISTER(Get_Device_SDR_Info);
-#endif
 
-#if 0 // Unimplemented.
 static void ipmicmd_Get_Device_SDR(IPMBSvc &ipmb, const IPMI_MSG &message) {
-	// Unimplemented.
+	if (message.data_len != 6)
+		RETURN_ERROR(ipmb, message, IPMI::Completion::Request_Data_Length_Invalid);
+	uint16_t reservation = (message.data[1] << 8) | message.data[0];
+	if (reservation && reservation != device_sdr_repo.get_current_reservation())
+		RETURN_ERROR(ipmb, message, IPMI::Completion::Reservation_Cancelled);
+	uint16_t record_id = (message.data[3] << 8) | message.data[2];
+	if (record_id == 0xFFFF)
+		record_id = device_sdr_repo.size() - 1;
+	std::shared_ptr<const SensorDataRecord> record = device_sdr_repo.get(record_id);
+	if (!record)
+		RETURN_ERROR(ipmb, message, IPMI::Completion::Requested_Sensor_Data_Or_Record_Not_Present);
+	uint16_t next_record = record_id + 1;
+	if (next_record >= device_sdr_repo.size())
+		next_record = 0xFFFF;
+	std::vector<uint8_t> reply{IPMI::Completion::Success, static_cast<uint8_t>(next_record & 0xFF), static_cast<uint8_t>(next_record >> 8)};
+	std::vector<uint8_t> sdrdata = record->u8export(message.rsSA, 0);
+	int i = message.data[4]; // offset
+	int limit = message.data[4] + message.data[5] /* bytes to read */;
+	if (limit > sdrdata.size())
+		limit = sdrdata.size();
+	for (; i < limit; ++i)
+		reply.push_back(sdrdata[i]);
+	if (reply.size() > IPMI_MSG::max_data_len)
+		RETURN_ERROR(ipmb, message, IPMI::Completion::Cannot_Return_Requested_Number_Of_Data_Bytes);
+	ipmb.send(message.prepare_reply(reply));
 }
 IPMICMD_INDEX_REGISTER(Get_Device_SDR);
-#endif
 
-#if 0 // Unimplemented.
 static void ipmicmd_Reserve_Device_SDR_Repository(IPMBSvc &ipmb, const IPMI_MSG &message) {
-	// Unimplemented.
+	uint16_t reservation = device_sdr_repo.reserve();
+	ipmb.send(message.prepare_reply({IPMI::Completion::Success, static_cast<uint8_t>(reservation & 0xFF), static_cast<uint8_t>(reservation >> 8)}));
 }
 IPMICMD_INDEX_REGISTER(Reserve_Device_SDR_Repository);
-#endif
 
 #if 0 // Unimplemented.
 static void ipmicmd_Get_Sensor_Reading_Factors(IPMBSvc &ipmb, const IPMI_MSG &message) {
