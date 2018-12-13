b51331ffa126fcefe8e377744b2fd9243f52fa73
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2018-12-13 11:42:52 -0600

    Multitude improvements & bugfixes to SensorDataRepository
    Reservations are now properly handled by the modification commands, and
    properly supported (optionally) by the access commands.
    
    Reservation failures are now exceptions rather than return values,
    allowing u8import() to return false in other conditions unambiguously.
    
    Checksums have been added to the SensorDatRepository export format,
    ensuring that the correct version and contents of a repository are
    reloaded from persistent storage.

--- uw-ipmc/services/ipmi/sdr/SensorDataRepository.cpp
+++ uw-ipmc/services/ipmi/sdr/SensorDataRepository.cpp
@@ -7,6 +7,7 @@
 
 #include <services/ipmi/sdr/SensorDataRepository.h>
 #include <libs/ThreadingPrimitives.h>
+#include <services/ipmi/IPMI_MSG.h> // for ipmi_checksum()
 
 SensorDataRepository::SensorDataRepository()
 	: reservation(0), last_update_ts(0) {
@@ -28,14 +29,11 @@ SensorDataRepository::~SensorDataRepository() {
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::add(const SensorDataRecord &record, uint8_t reservation) {
+void SensorDataRepository::add(const SensorDataRecord &record, reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
 	/* Ensure we have our own copy, and that it is valid.
 	 * Retrievals are const, and we want modifications to be done by replacement.
 	 */
@@ -58,7 +56,6 @@ bool SensorDataRepository::add(const SensorDataRecord &record, uint8_t reservati
 			this->last_update_ts = time(NULL);
 		}
 	}
-	return true;
 }
 
 /**
@@ -68,17 +65,13 @@ bool SensorDataRepository::add(const SensorDataRecord &record, uint8_t reservati
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::add(const SensorDataRepository &sdrepository, uint8_t reservation) {
+void SensorDataRepository::add(const SensorDataRepository &sdrepository, reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
 	for (auto it = sdrepository.records.begin(), eit = sdrepository.records.end(); it != eit; ++it)
 		this->add(**it, reservation);
-	return true;
 }
 
 /**
@@ -88,19 +81,15 @@ bool SensorDataRepository::add(const SensorDataRepository &sdrepository, uint8_t
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::remove(uint16_t id, uint8_t reservation) {
+void SensorDataRepository::remove(uint16_t id, reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
 	if (id <= this->records.size())
 		this->records.erase(std::next(this->records.begin(), id));
 	this->renumber();
 	this->last_update_ts = time(NULL);
-	return true;
 }
 
 /**
@@ -111,14 +100,11 @@ bool SensorDataRepository::remove(uint16_t id, uint8_t reservation) {
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::remove(const SensorDataRecord &record, uint8_t reservation) {
+void SensorDataRepository::remove(const SensorDataRecord &record, reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
 	for (auto it = this->records.begin(); it != this->records.end(); /* below */) {
 		if (**it == record)
 			it = this->records.erase(it);
@@ -127,7 +113,6 @@ bool SensorDataRepository::remove(const SensorDataRecord &record, uint8_t reserv
 	}
 	this->renumber();
 	this->last_update_ts = time(NULL);
-	return true;
 }
 
 /**
@@ -136,17 +121,13 @@ bool SensorDataRepository::remove(const SensorDataRecord &record, uint8_t reserv
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::clear(uint8_t reservation) {
+void SensorDataRepository::clear(reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
 	this->records.clear();
 	this->last_update_ts = time(NULL);
-	return true;
 }
 
 /**
@@ -171,9 +152,12 @@ time_t SensorDataRepository::last_update_timestamp() {
  *
  * @param id The record id of the record to retrieve
  * @return A std::shared_ptr<SensorDataRecord> containing a specialized SDR subclass.
+ * @throw reservation_cancelled_error
  */
-std::shared_ptr<const SensorDataRecord> SensorDataRepository::get(uint16_t id) const {
+std::shared_ptr<const SensorDataRecord> SensorDataRepository::get(uint16_t id, reservation_t reservation) const {
 	MutexGuard<true> lock(this->mutex, true);
+	if (reservation && reservation != this->reservation)
+		throw reservation_cancelled_error("The provided reservation is not valid");
 	if (id >= this->records.size())
 		return NULL;
 	return this->records.at(id);
@@ -184,9 +168,12 @@ std::shared_ptr<const SensorDataRecord> SensorDataRepository::get(uint16_t id) c
  *
  * @param key The record key bytes of the record to retrieve.
  * @return A std::shared_ptr<SensorDataRecord> containing a specialized SDR subclass.
+ * @throw reservation_cancelled_error
  */
-std::shared_ptr<const SensorDataRecord> SensorDataRepository::find(const std::vector<uint8_t> &key) const {
+std::shared_ptr<const SensorDataRecord> SensorDataRepository::find(const std::vector<uint8_t> &key, reservation_t reservation) const {
 	MutexGuard<true> lock(this->mutex, true);
+	if (reservation && reservation != this->reservation)
+		throw reservation_cancelled_error("The provided reservation is not valid");
 	for (auto it = this->records.begin(), eit = this->records.end(); it != eit; ++it)
 		if ((*it)->record_key() == key)
 			return *it;
@@ -243,48 +230,59 @@ std::vector<uint8_t> SensorDataRepository::u8export() const {
 		output.push_back(record.sdr_data.size());
 		output.insert(output.end(), record.sdr_data.begin(), record.sdr_data.end());
 	}
+	output.insert(output.begin(), ipmi_checksum(output));
 	return output;
 }
 
 /**
  * Import the data from the supplied u8 vector, merging with this SDR repository.
  *
- * \note Invalid records are silently discarded.
+ * \note Invalid records are silently discarded, but synchronization, format
+ *       or checksum errors are detectable.  If false is returned, no changes to
+ *       the repository will have been made.
  *
  * @param data The SDR repository contents in binary form
  * @param reservation The current reservation (or 0 to oneshot reserve for this
  *                    operation).  If this does not match the current
  *                    reservation, the operation will not complete.
- * @return true if the operation was executed, false if the reservation was invalid
+ * @return true if the operation was successful, false if data was invalid
+ * @throw reservation_cancelled_error
  */
-bool SensorDataRepository::u8import(const std::vector<uint8_t> &data, uint8_t reservation) {
+bool SensorDataRepository::u8import(const std::vector<uint8_t> &data, reservation_t reservation) {
 	MutexGuard<true> lock(this->mutex, true);
-	if (!reservation)
-		reservation = this->reserve();
-	if (reservation != this->reservation)
-		return false;
+	this->assert_reservation(reservation);
+
+	if (data.size() < 2)
+		return false; // No data here.
+	if (ipmi_checksum(data))
+		return false; // Invalid checksum.
 
 	// Retrieve Update Timestamp
 	if (data.size() < sizeof(time_t))
-		return true;
-	std::vector<uint8_t>::size_type cur = 0;
+		return false;
+	std::vector<uint8_t>::size_type cur = 1;
 	time_t last_update_ts = 0;
 	for (; cur < sizeof(time_t); ++cur)
 		last_update_ts |= data[cur] << (cur*8);
 	if (last_update_ts > this->last_update_ts)
 		this->last_update_ts = last_update_ts;
 
+	SensorDataRepository tmprepo;
+
 	// Import Records
 	while (cur < data.size()) {
 		uint8_t record_length = data[cur];
 		if (record_length == 0)
-			break; // We're out of sync. Abort.
+			return false; // Synchronization invalid
+		if (cur+1+record_length > data.size())
+			return false; // Synchronization invalid
 		std::vector<uint8_t> sdrdata(std::next(data.begin(), cur+1), std::next(data.begin(), cur+1+record_length));
 		cur += 1+record_length;
 		std::shared_ptr<SensorDataRecord> sdr = SensorDataRecord::interpret(sdrdata);
 		if (sdr)
-			this->add(*sdr, reservation);
+			tmprepo.add(*sdr, 0);
 	}
+	this->add(tmprepo, reservation);
 	return true;
 }
 
@@ -316,4 +314,17 @@ void SensorDataRepository::renumber() {
 	MutexGuard<true> lock(this->mutex, true);
 	for (std::vector< std::shared_ptr<SensorDataRecord> >::size_type i = 0; i < this->records.size(); ++i)
 		this->records.at(i)->record_id(i);
+}
+
+/**
+ * Acquire or verify a supplied reservation
+ * @param reservation The reservation to acquire or verify
+ * @throw reservation_cancelled_error
+ */
+void SensorDataRepository::assert_reservation(reservation_t &reservation) {
+	MutexGuard<true> lock(this->mutex, true);
+	if (!reservation)
+		reservation = this->reserve();
+	if (reservation != this->reservation)
+		throw reservation_cancelled_error("The provided reservation is not valid");
 }
