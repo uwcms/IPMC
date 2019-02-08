7a50909a9169706254dd6c71560d8209119bc69a
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2019-02-08 17:09:34 -0600

    Submodulize ip_repo and common

--- Vivado/ip_repo/ad7689_s_1.0/drivers/ad7689_s_v1_0/data/ad7689_s.tcl
+++
@@ -1,7 +0,0 @@
-
-
-proc generate {drv_handle} {
-    ::hsi::utils::define_include_file $drv_handle "xparameters.h" "ad7689_s" "NUM_INSTANCES" "DEVICE_ID"  "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_SLAVES"
-    ::hsi::utils::define_config_file $drv_handle "ad7689_s_g.c" "AD7689_S" "DEVICE_ID"  "C_S_AXI_BASEADDR" "C_SLAVES"
-    ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "ad7689_s" "NUM_INSTANCES" "DEVICE_ID"  "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_SLAVES"
-}
