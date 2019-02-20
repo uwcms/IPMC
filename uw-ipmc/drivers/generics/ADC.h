5e82c05da7e30cf809fb420107162c37d74afbd2
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2019-02-20 16:48:44 -0600

    Require ADC::Channel provide a bidirectional unit conversion function.
    This will be used when programming sensor thresholds into the sensor
    processing IP.

--- uw-ipmc/drivers/generics/ADC.h
+++ uw-ipmc/drivers/generics/ADC.h
@@ -18,17 +18,26 @@ public:
 	public:
 		using Callback = float (*)(float);
 		Channel(ADC& adc, uint8_t channel, float factor = 1.0) : adc(adc), channel(channel), factor(factor) {};
-		Channel(ADC& adc, uint8_t channel, Callback callback) : adc(adc), channel(channel), callback(callback) {};
+		Channel(ADC& adc, uint8_t channel, Callback callback, Callback callback_reverse) : adc(adc), channel(channel), callback(callback), callback_reverse(callback_reverse) { };
 		explicit operator float() {
-			float r = this->adc.getReading(this->channel);
-			if (this->callback) return this->callback(r);
-			else return r * this->factor;
+			return convert_from_raw(this->adc.getReading(this->channel));
 		};
+		float convert_from_raw(float r) {
+			if (this->callback)
+				return this->callback(r);
+			return r * this->factor;
+		};
+		float convert_to_raw(float v) {
+			if (this->callback_reverse)
+				return this->callback_reverse(v);
+			return v / this->factor;
+		}
 		inline uint8_t getChannel() { return this->channel; };
 	private:
 		ADC& adc;
 		uint8_t channel;
 		Callback callback = nullptr;
+		Callback callback_reverse = nullptr;
 		float factor = 1.0;
 	};
 };
