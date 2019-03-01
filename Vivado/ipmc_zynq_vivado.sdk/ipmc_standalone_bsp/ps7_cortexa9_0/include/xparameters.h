064b8b630110beced9c9845ce7a3221c4f059eac
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2019-03-01 13:02:26 -0600

    Enable xilffs in BSP for FSBL

--- Vivado/ipmc_zynq_vivado.sdk/ipmc_standalone_bsp/ps7_cortexa9_0/include/xparameters.h
+++ Vivado/ipmc_zynq_vivado.sdk/ipmc_standalone_bsp/ps7_cortexa9_0/include/xparameters.h
@@ -774,4 +774,10 @@
 
 /******************************************************************/
 
+/* Xilinx FAT File System Library (XilFFs) User Settings */
+#define FILE_SYSTEM_USE_MKFS
+#define FILE_SYSTEM_NUM_LOGIC_VOL 2
+#define FILE_SYSTEM_USE_STRFUNC 0
+#define FILE_SYSTEM_SET_FS_RPATH 0
+#define FILE_SYSTEM_WORD_ACCESS
 #endif  /* end of protection macro */
