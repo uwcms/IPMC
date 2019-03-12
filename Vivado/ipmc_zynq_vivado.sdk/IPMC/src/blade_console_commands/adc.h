4c1445a5df27bcc28d22330fbdea76b9436bf640
Marcelo Vicente <marcelo.vicente@cern.ch>
2019-03-12 09:10:47 -0500

    Improves APd1 ADC display

--- Vivado/ipmc_zynq_vivado.sdk/IPMC/src/blade_console_commands/adc.h
+++ Vivado/ipmc_zynq_vivado.sdk/IPMC/src/blade_console_commands/adc.h
@@ -1,3 +1,6 @@
+#include <libs/ANSICode.h>
+#include <map>
+
 namespace {
 
 /// A "version" console command.
@@ -11,9 +14,17 @@ public:
 
 	virtual void execute(std::shared_ptr<ConsoleSvc> console, const CommandParser::CommandParameters &parameters) {
 
+		const std::string RED = ANSICode::color(ANSICode::WHITE, ANSICode::RED, true);
+		const std::string YELLOW = ANSICode::color(ANSICode::YELLOW, ANSICode::NOCOLOR, true);
+		const std::string NORMAL = ANSICode::color();
+
+		std::map<char, std::string> uname = {{'v', "Volts"}, {'c', "Celsius"}, {'i', "Ampere"}};
+
 		struct ChannelEntry {
-			std::string name;
-			std::string unit;
+			const char* name;
+			char unit;
+			float nominal;
+			float tolerance;
 			int intf;
 			int slave;
 			ADC::Channel adc;
@@ -24,46 +35,48 @@ public:
 		ADC::Channel::Callback imon    = [](float r) -> float { return r * 1000.0 / 80.0; };
 		ADC::Channel::Callback imon_r  = [](float r) -> float { return r / 1000.0 * 80.0; };
 
-		std::vector< ChannelEntry > channels = {
-			{"+12VPYLD      ", "Volt", 0, 0, ADC::Channel(*adc[0], 0, 5.640)},
-			{"+5VPYLD       ", "Volt", 0, 0, ADC::Channel(*adc[0], 1, 2.467)},
-			{"+3.55VBULK    ", "Volt", 0, 0, ADC::Channel(*adc[0], 2, 1.749)},
-			{"+3.3VMP2      ", "Volt", 0, 0, ADC::Channel(*adc[0], 3, 1.649)},
-			{"+3.3VFFLY5    ", "Volt", 0, 0, ADC::Channel(*adc[0], 4, 1.649)},
-			{"+3.3VFFLY4    ", "Volt", 0, 0, ADC::Channel(*adc[0], 5, 1.649)},
-			{"+3.3VFFLY3    ", "Volt", 0, 0, ADC::Channel(*adc[0], 6, 1.649)},
-			{"+3.3VFFLY2    ", "Volt", 0, 0, ADC::Channel(*adc[0], 7, 1.649)},
+		ChannelEntry channels[] = {
+			{"+12VPYLD      ", 'v', 12.00, 0.05, 0, 0, ADC::Channel(*adc[0], 0, 5.640)},
+			{"+5VPYLD       ", 'v',  5.00, 0.05, 0, 0, ADC::Channel(*adc[0], 1, 2.467)},
+			{"+3.3VDD       ", 'v',  3.30, 0.05, 1, 0, ADC::Channel(*adc[1], 6, 1.649)},
+			{"+1.8VDD       ", 'v',  1.80, 0.05, 1, 0, ADC::Channel(*adc[1], 1)},
+			{"+3.3VMP2      ", 'v',  3.30, 0.05, 0, 0, ADC::Channel(*adc[0], 3, 1.649)},
 
-			{"+3.3VFFLY1    ", "Volt", 1, 0, ADC::Channel(*adc[1], 7, 1.649)},
-			{"+3.3VDD       ", "Volt", 1, 0, ADC::Channel(*adc[1], 6, 1.649)},
-			{"+2.5VXPT      ", "Volt", 1, 0, ADC::Channel(*adc[1], 5, 1.299)},
-			{"+1.95VBULK    ", "Volt", 1, 0, ADC::Channel(*adc[1], 4)},
-			{"+1.8VFFLY2    ", "Volt", 1, 0, ADC::Channel(*adc[1], 3)},
-			{"+1.8VFFLY1    ", "Volt", 1, 0, ADC::Channel(*adc[1], 2)},
-			{"+1.8VDD       ", "Volt", 1, 0, ADC::Channel(*adc[1], 1)},
+			{"+2.5VXPT      ", 'v',  2.50, 0.05, 1, 0, ADC::Channel(*adc[1], 5, 1.299)},
+			{"+1.2VPHY      ", 'v',  1.20, 0.05, 2, 1, ADC::Channel(*adc[3], 2)},
+			{"VLUTVDDIO     ", 'v',  1.20, 0.05, 2, 1, ADC::Channel(*adc[3], 5)},
 
-			{"+0.9VMGTT     ", "Volt", 2, 0, ADC::Channel(*adc[2], 0)},
-			{"+0.9VMGTB     ", "Volt", 2, 0, ADC::Channel(*adc[2], 1)},
-			{"+0.85VDD      ", "Volt", 2, 0, ADC::Channel(*adc[2], 2)},
-			{"MGT0.5VT_IMON ", "Ampere", 2, 0, ADC::Channel(*adc[2], 3, imon, imon_r)},
-			{"MGT0.9VB_IMON ", "Ampere", 2, 0, ADC::Channel(*adc[2], 4, imon, imon_r)},
-			{"MGT1.2VT_IMON ", "Ampere", 2, 0, ADC::Channel(*adc[2], 5, imon, imon_r)},
-			{"MGT1.2VB_IMON ", "Ampere", 2, 0, ADC::Channel(*adc[2], 6, imon, imon_r)},
-			{"T_BOARD1      ", "Celsius", 2, 0, ADC::Channel(*adc[2], 7, tmp36, tmp36_r)},
+			{"+3.55VBULK    ", 'v',  3.55, 0.05, 0, 0, ADC::Channel(*adc[0], 2, 1.749)},
+			{"+1.95VBULK    ", 'v',  1.95, 0.05, 1, 0, ADC::Channel(*adc[1], 4)},
+			{"+0.85VDD      ", 'v',  0.85, 0.01, 2, 0, ADC::Channel(*adc[2], 2)},
 
-			{"+1.35VMGTT    ", "Volt", 2, 1, ADC::Channel(*adc[3], 0)},
-			{"+1.35VMGTB    ", "Volt", 2, 1, ADC::Channel(*adc[3], 1)},
-			{"+1.2VPHY      ", "Volt", 2, 1, ADC::Channel(*adc[3], 2)},
-			{"+1.2VMGTT     ", "Volt", 2, 1, ADC::Channel(*adc[3], 3)},
-			{"+1.2VMGTB     ", "Volt", 2, 1, ADC::Channel(*adc[3], 4)},
-			{"VLUTVDDIO     ", "Volt", 2, 1, ADC::Channel(*adc[3], 5)},
-			{"+1.05VMGTT    ", "Volt", 2, 1, ADC::Channel(*adc[3], 6)},
-			{"+1.05VMGTB    ", "Volt", 2, 1, ADC::Channel(*adc[3], 7)},
+			{"+1.35VMGTT    ", 'v',  1.35, 0.01, 2, 1, ADC::Channel(*adc[3], 0)},
+			{"+1.35VMGTB    ", 'v',  1.35, 0.01, 2, 1, ADC::Channel(*adc[3], 1)},
+			{"+1.2VMGTT     ", 'v',  1.20, 0.01, 2, 1, ADC::Channel(*adc[3], 3)},
+			{"+1.2VMGTB     ", 'v',  1.20, 0.01, 2, 1, ADC::Channel(*adc[3], 4)},
+			{"+1.05VMGTT    ", 'v',  1.05, 0.01, 2, 1, ADC::Channel(*adc[3], 6)},
+			{"+1.05VMGTB    ", 'v',  1.05, 0.01, 2, 1, ADC::Channel(*adc[3], 7)},
+			{"+0.9VMGTT     ", 'v',  0.90, 0.01, 2, 0, ADC::Channel(*adc[2], 0)},
+			{"+0.9VMGTB     ", 'v',  0.90, 0.01, 2, 0, ADC::Channel(*adc[2], 1)},
 
-			{"+1.8VFFLY3    ", "Volt", 2, 2, ADC::Channel(*adc[4], 0)},
-			{"+1.8VFFLY4    ", "Volt", 2, 2, ADC::Channel(*adc[4], 1)},
-			{"+1.8VFFLY5    ", "Volt", 2, 2, ADC::Channel(*adc[4], 2)},
-			{"T_BOARD2      ", "Celsius", 2, 2, ADC::Channel(*adc[4], 3, tmp36, tmp36_r)},
+			{"MGT0.9VT_IMON ", 'i', 10.00, 0.00, 2, 0, ADC::Channel(*adc[2], 3, imon, imon_r)},
+			{"MGT0.9VB_IMON ", 'i', 10.00, 0.00, 2, 0, ADC::Channel(*adc[2], 4, imon, imon_r)},
+			{"MGT1.2VT_IMON ", 'i', 10.00, 0.00, 2, 0, ADC::Channel(*adc[2], 5, imon, imon_r)},
+			{"MGT1.2VB_IMON ", 'i', 10.00, 0.00, 2, 0, ADC::Channel(*adc[2], 6, imon, imon_r)},
+
+			{"+3.3VFFLY1    ", 'v',  3.30, 0.05, 1, 0, ADC::Channel(*adc[1], 7, 1.649)},
+			{"+3.3VFFLY2    ", 'v',  3.30, 0.05, 0, 0, ADC::Channel(*adc[0], 7, 1.649)},
+			{"+3.3VFFLY3    ", 'v',  3.30, 0.05, 0, 0, ADC::Channel(*adc[0], 6, 1.649)},
+			{"+3.3VFFLY4    ", 'v',  3.30, 0.05, 0, 0, ADC::Channel(*adc[0], 5, 1.649)},
+			{"+3.3VFFLY5    ", 'v',  3.30, 0.05, 0, 0, ADC::Channel(*adc[0], 4, 1.649)},
+			{"+1.8VFFLY1    ", 'v',  1.80, 0.05, 1, 0, ADC::Channel(*adc[1], 2)},
+			{"+1.8VFFLY2    ", 'v',  1.80, 0.05, 1, 0, ADC::Channel(*adc[1], 3)},
+			{"+1.8VFFLY3    ", 'v',  1.80, 0.05, 2, 2, ADC::Channel(*adc[4], 0)},
+			{"+1.8VFFLY4    ", 'v',  1.80, 0.05, 2, 2, ADC::Channel(*adc[4], 1)},
+			{"+1.8VFFLY5    ", 'v',  1.80, 0.05, 2, 2, ADC::Channel(*adc[4], 2)},
+
+			{"T_BOARD1      ", 'c', 50.00, 0.00, 2, 0, ADC::Channel(*adc[2], 7, tmp36, tmp36_r)},
+			{"T_BOARD2      ", 'c', 50.00, 0.00, 2, 2, ADC::Channel(*adc[4], 3, tmp36, tmp36_r)},
 		};
 
 		console->write(stdsprintf("Zynq Temp: %0.2f Celsius\n", xadc->getTemperature()));
@@ -73,7 +86,31 @@ public:
 		}
 
 		for (auto &channel : channels) {
-			console->write(std::to_string(channel.intf) + "/" + std::to_string(channel.slave) + "/" + std::to_string(channel.adc.getChannel()) + " " + channel.name + " " + stdsprintf("%6.3f", (float)channel.adc) + " " + channel.unit + "\n");
+			float value = (float)(channel.adc);
+
+			std::string color = NORMAL;
+			switch (channel.unit) {
+			case 'v':
+				if ((value > (channel.nominal * (1.00 + channel.tolerance))) ||
+					(value < (channel.nominal * (1.00 - channel.tolerance)))) {
+					color = RED;
+				}
+				break;
+
+			case 'c':
+			case 'i':
+				if (value > channel.nominal) {
+					color = RED;
+				}
+				break;
+
+			default:
+				color = YELLOW;
+				break;
+			}
+
+			console->write(color + channel.name + " " + stdsprintf("%6.3f", value) + " " + uname[channel.unit] + NORMAL + "\n");
+			//console->write(std::to_string(channel.intf) + "/" + std::to_string(channel.slave) + "/" + std::to_string(channel.adc.getChannel()) + " " + channel.name + " " + stdsprintf("%6.3f", (float)channel.adc) + " " + channel.unit + "\n");
 		}
 	}
 };
