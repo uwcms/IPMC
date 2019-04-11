39b18382c0e29c600b4fdd08e45d75a55f6e04b8
Marcelo Vicente <marcelo.vicente@cern.ch>
2019-04-11 14:53:23 -0500

    Fixes improper addressing in SPI Flash driver

--- uw-ipmc/core_console_commands/flash.h
+++ uw-ipmc/core_console_commands/flash.h
@@ -2,26 +2,60 @@ namespace {
 
 class ConsoleCommand_flash_verify : public CommandParser::Command {
 public:
+
+	Flash &flash;
+
+	ConsoleCommand_flash_verify(Flash &flash) : flash(flash) {};
+
 	virtual std::string get_helptext(const std::string &command) const {
 		return stdsprintf(
-				"%s\n\n"
-				"Check if the image in the QSPI flash is valid.\n", command.c_str());
+				"%s $partition\n\n"
+				"Check if an image in the QSPI flash is valid.\n"
+				"Allowed partitions are:\n"
+				" IPMC revA: A\n"
+				" IPMC revB: fallback, A, B, test\n", command.c_str());
 	}
 
 	virtual void execute(std::shared_ptr<ConsoleSvc> console, const CommandParser::CommandParameters &parameters) {
-		std::unique_ptr<uint8_t> buf = std::unique_ptr<uint8_t>(new uint8_t[16 * 1024 * 1024]);
+#define MB * (1024 * 1024)
+		size_t address = 0;
 
-		if (!qspiflash->isInitialized()) qspiflash->initialize();
+		if (!flash.isInitialized()) flash.initialize();
 
-		qspiflash->read(0, &*buf, 16*1024*1024);
+		if (flash.getTotalSize() == (16 MB)) {
+			if (!parameters.parameters[1].compare("A")) {
+				address = 0;
+			} else {
+				console->write("IPMC revA only has image A, see help.\n");
+				return;
+			}
+		} else {
+			if (!parameters.parameters[1].compare("fallback")) {
+				address = 0;
+			} else if (!parameters.parameters[1].compare("A")) {
+				address = 16 MB;
+			} else if (!parameters.parameters[1].compare("B")) {
+				address = 32 MB;
+			} else if (!parameters.parameters[1].compare("test")) {
+				address = 48 MB;
+			} else {
+				console->write("Unknown image name, see help.\n");
+				return;
+			}
+		}
 
-		BootFileValidationReturn r = validateBootFile(&*buf, 16*1024*1024);
+		std::unique_ptr<uint8_t> buf = std::unique_ptr<uint8_t>(new uint8_t[16 MB]);
+
+		qspiflash->read(address, &*buf, 16 MB);
+
+		BootFileValidationReturn r = validateBootFile(&*buf, 16 MB);
 
 		if (r == 0) {
 			console->write("QSPI image is VALID\n", r);
 		} else {
-			console->write(stdsprintf("QSPI image is INVALID (reason: %d)\n", r));
+			console->write(stdsprintf("QSPI image is INVALID (reason: %s)\n", getBootFileValidationErrorString(r)));
 		}
+#undef MB
 	}
 };
 
